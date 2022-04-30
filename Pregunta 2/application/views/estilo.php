3e53624bb6d29453e716d4a7ad675226f240f5b9
Augusto Rios <56129071+SphinxBSD@users.noreply.github.com>
2022-04-29 23:40:48 -0400

    Add files via upload

---
+++ Pregunta 2/application/views/estilo.php
@@ -0,0 +1,29 @@
+<style>
+	section {
+    width: 75%;
+    float: left;
+    background-color: #e0e0e0;
+}
+
+aside {
+    width: 25%;
+    float: left;
+    background-color: #e0e0e0;
+    border-left: 1px solid white;
+}
+
+header {
+    padding: 25px;
+    background-color: #cfd8dc;
+}
+
+
+footer {
+    background-color: #CFD8DC;
+}
+
+
+.contenido {
+    display: flex;
+}
+</style>
